77badcabbf32112c8e35c128ba3ab01602e1f1f3
Dany Paredes <danywalls4@gmail.com>
2024-11-02 20:07:47 +0100

    use kendo-timeline component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, inject} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {KENDO_TIMELINE} from '@progress/kendo-angular-layout';
+import {GithubService} from './services/github.service';
+import {DatePipe} from "@angular/common";
+import {toSignal} from "@angular/core/rxjs-interop";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    imports: [RouterOutlet, KENDO_TIMELINE, DatePipe],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'github-timeline';
+
+    githubService = inject(GithubService);
+    public $commits = toSignal(this.githubService.getCommits('alyssamichelle', 'tour-of-ponies'));
+
+
 }
